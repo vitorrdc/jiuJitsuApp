e26e16afcc6af34f5355b7cde551e2b2132f598a
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-02-16 12:22:59 -0300

    add react native date picker

--- src/screens/Timeline.tsx
+++ src/screens/Timeline.tsx
@@ -5,7 +5,7 @@ export function Timeline() {
   return (
     <View className="flex-1 bg-background px-8 pt-16" >
       <Text className="text-white font-bold">
-        Timeline's screen
+        Minha Linha do Tempo
       </Text>
     </View>
   )
--- src/screens/ToSchedule.tsx
+++ src/screens/ToSchedule.tsx
@@ -1,12 +1,100 @@
-import {View, Text} from "react-native";
+import {View, Text, Button} from "react-native";
+import { BackButton } from "../components/BackButton";
+import { useState } from "react";
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 
 export function ToSchedule() {
 
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date: Date) => {
+    console.warn("Data selecionada: ", date);
+    hideDatePicker();
+  };
+
   return (
-    <View className="flex-1 bg-background px-8 pt-16" >
-      <Text className="text-white font-bold">
-        Schedule Screen
-      </Text>
+    <View className="flex-1 bg-background">
+      <View className="mt-6 ml-4">
+        <BackButton />
+      </View>
+      <View className="flex-1 px-8  items-center" >
+        <Text className="text-white font-bold text-xl">
+          Agenda de aulas
+        </Text>
+        <View className="bg-zinc-800 h-auto pb-10 w-80 mt-8 rounded-lg flex">
+          <Text className="text-white font-semibold text-xl mt-2 ml-auto mr-auto">
+            Hoje
+          </Text>
+          <View className="flex-row justify-around">
+            <View className="bg-background w-40 h-10 rounded-lg flex items-center justify-center mt-4">
+              <Text className="text-white">
+                08h00: às 07h00
+              </Text>
+            </View>
+            <TouchableOpacity 
+              className="bg-green-500 h-10 w-28 flex items-center justify-center rounded-lg mt-4"
+              activeOpacity={0.85}
+            >
+              <Text className="text-white font-semibold">
+                Confirmar
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-around">
+            <View className="bg-background w-40 h-10 rounded-lg flex items-center justify-center mt-4">
+              <Text className="text-white">
+                18h15: às 19h30
+              </Text>
+            </View>
+            <TouchableOpacity 
+              className="bg-green-500 h-10 w-28 flex items-center justify-center rounded-lg mt-4"
+              activeOpacity={0.85}
+            >
+              <Text className="text-white font-semibold">
+                Confirmar
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-around">
+            <View className="bg-background w-40 h-10 rounded-lg flex items-center justify-center mt-4">
+              <Text className="text-white">
+                20h00 às 22h00
+              </Text>
+            </View>
+            <TouchableOpacity 
+              className="bg-green-500 h-10 w-28 flex items-center justify-center rounded-lg mt-4"
+              activeOpacity={0.85}
+            >
+              <Text className="text-white font-semibold">
+                Confirmar
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="mt-6">
+        <Button 
+          title="Agendar data futura" 
+          onPress={showDatePicker}
+        />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+          buttonTextColorIOS='white'
+        />
+    </View>
+      </View>
     </View>
   )
 }
