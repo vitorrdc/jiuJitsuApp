4c49696e369ec9161279e6394df19b3591b0a3a2
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-03-13 23:44:45 -0300

    started confirm attendance improves

--- src/screens/ConfirmAttendance.tsx
+++ src/screens/ConfirmAttendance.tsx
@@ -1,40 +1,56 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
-import { BackButton } from '../components/BackButton'
+import { VStack, Text, Button, Modal } from 'native-base'
+import { useState } from 'react'
 
 export function ConfirmAttendance() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <View className="flex-1 bg-background items-center p-4">
-      <View className="flex-row w-full justify-center mb-8 mt-6">
-        <View className="absolute left-0">
-          <BackButton />
-        </View>
-        <Text className="text-white font-bold text-xl">
-          Confirmar Presenças
-        </Text>
-      </View>
-      <View className="w-full bg-zinc-600 items-center rounded-lg p-4">
-        <Text className="text-white font-semibold text-base mb-4">
-          10/03/2023 - 18:15h às 19:30h
-        </Text>
-        <View className="w-full bg-background border border-zinc-400 rounded-lg p-2 flex-row justify-between mb-2">
-          <Text className="text-white text-base">Vitor Ribeiro</Text>
-          <TouchableOpacity className="bg-green-500 rounded-lg w-8 h-6" />
-        </View>
-        <View className="w-full bg-background border border-zinc-400 rounded-lg p-2 flex-row justify-between mb-2">
-          <Text className="text-white text-base">Danilo Walter</Text>
-          <TouchableOpacity className="bg-green-500 rounded-lg w-8 h-6" />
-        </View>
-        <View className="w-full bg-background border border-zinc-400 rounded-lg p-2 flex-row justify-between mb-2">
-          <Text className="text-white text-base">Eduardo</Text>
-          <TouchableOpacity className="bg-zinc-500 rounded-lg w-8 h-6" />
-        </View>
-        <TouchableOpacity className="bg-green-500 rounded-lg w-full p-2">
-          <Text className="text-white font-semibold text-center text-lg">
-            Finalizar conferência
+    <VStack backgroundColor="gray.900" flex={1} px={6}>
+      <Text
+        color="white"
+        fontFamily="heading"
+        fontSize="xl"
+        textAlign="center"
+        mt={6}
+      >
+        Confirmar presença
+      </Text>
+      <VStack w="full" backgroundColor="gray.400" mt={10}>
+        <Button onPress={() => setShowModal(true)} bgColor="blue.500">
+          <Text color="white" fontFamily="heading">
+            10/03/2023 - 18:15h às 19:30h
           </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+        </Button>
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Contact Us</Modal.Header>
+            <Modal.Body>
+              <Text>TESTE </Text>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button.Group space={2}>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowModal(false)
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onPress={() => {
+                    setShowModal(false)
+                  }}
+                >
+                  Save
+                </Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </VStack>
+    </VStack>
   )
 }
