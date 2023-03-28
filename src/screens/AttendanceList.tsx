bed1176f7191ff0bc12dc533e4fc58b33dc4f837
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-03-28 20:22:20 -0300

    add search engine in Attendance List

--- src/screens/AttendanceList.tsx
+++ src/screens/AttendanceList.tsx
@@ -2,17 +2,120 @@ import {
   Center,
   VStack,
   Heading,
-  ScrollView,
   Button,
   Modal,
   Stack,
   Text,
+  Input,
+  FlatList,
+  ScrollView,
 } from 'native-base'
 import { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export function AttendanceList() {
   const [showModal, setShowModal] = useState(false)
-  const students: string[] = ['Vitinho', 'Danilo', 'Davi', 'Wesley']
+  const [students, setStudents] = useState<string[]>([
+    'Vitinho',
+    'Danilo',
+    'Davi',
+    'Wesley',
+    'Fernando',
+    'Batata',
+    'Patati',
+    'Patata',
+    'Ogro',
+    'Jhonson',
+    'Wedsley',
+    'Joaquim',
+    'Maria',
+    'Grazielle',
+    'Júlia',
+    'Tonho',
+  ])
+  const [originalsStudents] = useState<string[]>([
+    'Vitinho',
+    'Danilo',
+    'Davi',
+    'Wesley',
+    'Fernando',
+    'Batata',
+    'Patati',
+    'Patata',
+    'Ogro',
+    'Jhonson',
+    'Wedsley',
+    'Joaquim',
+    'Maria',
+    'Grazielle',
+    'Júlia',
+    'Tonho',
+  ])
+
+  function renderList(item: string) {
+    return (
+      <Center>
+        <Button
+          onPress={() => setShowModal(true)}
+          width="full"
+          backgroundColor="blue.500"
+          mt={3}
+        >
+          {item}
+        </Button>
+        <Modal
+          isOpen={showModal}
+          onClose={() => setShowModal(false)}
+          overlayVisible={true}
+          backdropVisible={true}
+        >
+          <Modal.Content maxWidth="400px" backgroundColor="gray.200">
+            <Modal.CloseButton />
+            <Modal.Header backgroundColor="gray.200">
+              <Text fontWeight="bold" fontSize="lg">
+                Presença por faixa
+              </Text>
+            </Modal.Header>
+            <Modal.Body>
+              <Stack
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+              >
+                <Text>Faixa branca</Text>
+                <Text>135 aulas</Text>
+              </Stack>
+              <Stack
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+              >
+                <Text>Faixa azul</Text>
+                <Text>87 aulas</Text>
+              </Stack>
+            </Modal.Body>
+            <Modal.Footer backgroundColor="gray.200">
+              <Button.Group space={2}>
+                <Button
+                  onPress={() => {
+                    setShowModal(false)
+                  }}
+                  backgroundColor="gray.900"
+                >
+                  Fechar
+                </Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </Center>
+    )
+  }
+
+  function searchItem(item: string) {
+    const filtereds = JSON.parse(JSON.stringify(originalsStudents))
+    setStudents(filtereds.filter((student: string) => student.includes(item)))
+  }
 
   return (
     <VStack flex={1} backgroundColor="gray.900" p={6}>
@@ -21,62 +124,24 @@ export function AttendanceList() {
           Presenças Gerais
         </Heading>
       </Center>
-      <ScrollView showsVerticalScrollIndicator={false} paddingBottom={100}>
-        {students.map((student, index) => {
-          return (
-            <Center key={index}>
-              <Button
-                onPress={() => setShowModal(true)}
-                width="full"
-                backgroundColor="blue.500"
-                mt={3}
-              >
-                {student}
-              </Button>
-              <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-                <Modal.Content maxWidth="400px" backgroundColor="gray.200">
-                  <Modal.CloseButton />
-                  <Modal.Header backgroundColor="gray.200">
-                    <Text fontWeight="bold" fontSize="lg">
-                      Presença por faixa
-                    </Text>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <Stack
-                      display="flex"
-                      flexDirection="row"
-                      justifyContent="space-between"
-                    >
-                      <Text>Faixa branca</Text>
-                      <Text>135 aulas</Text>
-                    </Stack>
-                    <Stack
-                      display="flex"
-                      flexDirection="row"
-                      justifyContent="space-between"
-                    >
-                      <Text>Faixa azul</Text>
-                      <Text>87 aulas</Text>
-                    </Stack>
-                  </Modal.Body>
-                  <Modal.Footer backgroundColor="gray.200">
-                    <Button.Group space={2}>
-                      <Button
-                        onPress={() => {
-                          setShowModal(false)
-                        }}
-                        backgroundColor="gray.900"
-                      >
-                        Fechar
-                      </Button>
-                    </Button.Group>
-                  </Modal.Footer>
-                </Modal.Content>
-              </Modal>
-            </Center>
-          )
-        })}
-      </ScrollView>
+      <Stack mb={8}>
+        <Input
+          mt={6}
+          backgroundColor="gray.400"
+          borderColor="gray.300"
+          placeholder="Buscar aluno"
+          onChangeText={(item) => searchItem(item)}
+          color="white"
+        />
+      </Stack>
+
+      <SafeAreaView>
+        <FlatList
+          data={students}
+          renderItem={({ item }) => renderList(item)}
+          keyExtractor={(item) => String(item)}
+        />
+      </SafeAreaView>
     </VStack>
   )
 }
