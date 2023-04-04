e58586bc42e9e3c0fea2e485839a9891f0b23f32
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-04-03 23:19:05 -0300

    finished general attendance list

--- src/screens/AttendanceList.tsx
+++ src/screens/AttendanceList.tsx
@@ -7,90 +7,66 @@ import {
   Stack,
   Text,
   Input,
-  FlatList,
+  ScrollView,
 } from 'native-base'
 import { useState } from 'react'
 
+type StudentsData = {
+  nome: string
+  faixa_branca: string
+  faixa_azul: string
+}
+
 export function AttendanceList() {
   const [showModal, setShowModal] = useState(false)
-  const [students, setStudents] = useState<string[]>([
-    'Vitinho',
-    'Danilo',
-    'Davi',
-    'Wesley',
+  const [students, setStudents] = useState<StudentsData[]>([
+    {
+      nome: 'Vitor',
+      faixa_branca: '180 aulas',
+      faixa_azul: '10 aulas',
+    },
+    {
+      nome: 'Fernando',
+      faixa_branca: '120 aulas',
+      faixa_azul: '16 aulas',
+    },
+    {
+      nome: 'Lucas',
+      faixa_branca: '135 aulas',
+      faixa_azul: '127 aulas',
+    },
   ])
-  const [originalsStudents] = useState<string[]>([
-    'Vitinho',
-    'Danilo',
-    'Davi',
-    'Wesley',
+  const [originalsStudents] = useState<StudentsData[]>([
+    {
+      nome: 'Vitor',
+      faixa_branca: '180 aulas',
+      faixa_azul: '10 aulas',
+    },
+    {
+      nome: 'Fernando',
+      faixa_branca: '120 aulas',
+      faixa_azul: '16 aulas',
+    },
+    {
+      nome: 'Lucas',
+      faixa_branca: '135 aulas',
+      faixa_azul: '127 aulas',
+    },
   ])
+  const [studentsData, setStudentsData] = useState<StudentsData>(
+    {} as StudentsData,
+  )
 
-  console.log(students[2])
-
-  function renderList(item: string, index: number) {
-    return (
-      <Center>
-        <Button
-          onPress={() => setShowModal(true)}
-          width="full"
-          backgroundColor="blue.500"
-          mt={3}
-        >
-          {item}
-        </Button>
-        <Modal
-          isOpen={showModal}
-          onClose={() => setShowModal(false)}
-          overlayVisible={true}
-          backdropVisible={true}
-        >
-          <Modal.Content maxWidth="400px" backgroundColor="gray.200">
-            <Modal.CloseButton />
-            <Modal.Header backgroundColor="gray.200">
-              <Text fontWeight="bold" fontSize="lg">
-                Presença por faixa: {item}
-              </Text>
-            </Modal.Header>
-            <Modal.Body>
-              <Stack
-                display="flex"
-                flexDirection="row"
-                justifyContent="space-between"
-              >
-                <Text>Faixa branca</Text>
-                <Text>135 aulas</Text>
-              </Stack>
-              <Stack
-                display="flex"
-                flexDirection="row"
-                justifyContent="space-between"
-              >
-                <Text>Faixa azul</Text>
-                <Text>87 aulas</Text>
-              </Stack>
-            </Modal.Body>
-            <Modal.Footer backgroundColor="gray.200">
-              <Button.Group space={2}>
-                <Button
-                  onPress={() => {
-                    setShowModal(false)
-                  }}
-                  backgroundColor="gray.900"
-                >
-                  Fechar
-                </Button>
-              </Button.Group>
-            </Modal.Footer>
-          </Modal.Content>
-        </Modal>
-      </Center>
-    )
+  function handleSetModalVisibility(data?: StudentsData | any) {
+    setShowModal(true)
+    setStudentsData(data)
   }
 
   function searchItem(item: string) {
     const filtereds = JSON.parse(JSON.stringify(originalsStudents))
-    setStudents(filtereds.filter((student: string) => student.includes(item)))
+    setStudents(
+      filtereds.filter((student: StudentsData) => student.nome.includes(item)),
+    )
   }
 
   return (
@@ -111,11 +87,74 @@ export function AttendanceList() {
           _focus={{ borderColor: 'gray.200' }}
         />
       </Stack>
-      <FlatList
-        data={students}
-        renderItem={({ item, index }) => renderList(item, index)}
-        keyExtractor={(item) => String(item)}
-      />
+      <VStack>
+        <Center>
+          <ScrollView
+            width="full"
+            showsVerticalScrollIndicator={false}
+            paddingBottom={100}
+          >
+            {students.map((data, index) => {
+              return (
+                <Button
+                  onPress={() => handleSetModalVisibility(data)}
+                  width="full"
+                  backgroundColor="blue.500"
+                  mt={3}
+                  key={index}
+                >
+                  {data.nome}
+                </Button>
+              )
+            })}
+          </ScrollView>
+          <Modal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            overlayVisible={true}
+            backdropVisible={true}
+          >
+            <Modal.Content maxWidth="400px" backgroundColor="gray.200">
+              <Modal.CloseButton />
+              <Modal.Header backgroundColor="gray.200">
+                <Text fontWeight="bold" fontSize="lg">
+                  Presença por faixa:
+                </Text>
+              </Modal.Header>
+              <Modal.Body>
+                <Stack
+                  display="flex"
+                  flexDirection="row"
+                  justifyContent="space-between"
+                >
+                  <Text>Faixa branca</Text>
+                  <Text>{studentsData.faixa_branca}</Text>
+                </Stack>
+                <Stack
+                  display="flex"
+                  flexDirection="row"
+                  justifyContent="space-between"
+                >
+                  <Text>Faixa azul</Text>
+                  <Text>{studentsData.faixa_azul}</Text>
+                </Stack>
+              </Modal.Body>
+              <Modal.Footer backgroundColor="gray.200">
+                <Button.Group space={2}>
+                  <Button
+                    onPress={() => {
+                      setShowModal(false)
+                    }}
+                    backgroundColor="gray.900"
+                  >
+                    Fechar
+                  </Button>
+                </Button.Group>
+              </Modal.Footer>
+            </Modal.Content>
+          </Modal>
+        </Center>
+      </VStack>
     </VStack>
   )
 }
